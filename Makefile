3e35a09ce3bf1daa1cf10e819f3b73dbdf633442
Hissirou <fodehissirou@gmail.com>
2017-04-12 16:38:00 +0200

    Ajoute des fichiers

---
+++ Makefile
@@ -0,0 +1,11 @@
+all: start
+
+start :
+	g++ -Wall -g p4vm.c -o play
+
+
+
+
+
+
+
---
+++ p4vm.h
@@ -0,0 +1,66 @@
+# include <stdio.h>
+# include <assert.h>
+# include <ctype.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+
+enum {
+  R = 1, J = 2,			// joueurs (Rouge et Jaune) et marques
+  Vide = 0,			// pas marque
+
+  N = 7,			// taille du damier : N nombre de colonne, M nombre de rangées
+  M = 6,
+  Xwin = 1, Xloose = -1, Nul = 0, // résultats
+  Fini = -1,
+
+  Rate = -1,			// case invalide
+
+
+  MaxCoup = 42,   // il y a 21 pions par joueur
+ 
+  MaxStrGrille = 1024,
+  MaxStrCoup = 100,
+};
+
+struct Grille
+{
+	int kikijoue ;
+	int place[N*M] ; 
+};
+
+struct Coup {
+  int ou;
+  int ki;
+};
+
+typedef struct Grille Grille;
+typedef struct Coup Coup ; 
+
+Grille mygrille ;
+
+
+
+/****** PROTOTYPES DES FONCTIONS ***********/
+
+
+char * strgrille(Grille * g) ; 
+static inline int lc2p(int l, int c) ; 
+
+
+void liregrille(Grille * g, FILE * fd) ; 
+static inline int mot2val(char * mot) ;
+static inline char * xgetline(FILE * oulire) ; 
+
+
+char * strcoup(Coup * coup) ; 
+Coup lirecoup(char * line) ; 
+static inline int autre(int ki) ; 
+void jouer(Grille * g, Coup * c) ; 
+void dejouer(Grille * g, Coup * c) ;
+
+
+int mkcoup(Grille * g, Coup c[]) ; 
+void strcfini(int val) ; 
+int cfini(Grille * g) ; 
