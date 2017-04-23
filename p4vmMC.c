cc294d4714ef1fcb756e07302da8c84fafceb008
Roxane <roxane_desrousseaux@hotmail.com>
2017-04-23 14:40:55 +0000

    Clean up

--- p4vmMC.c
+++ p4vmMC.c
@@ -13,54 +13,8 @@
 */
 
 #include "p4vm.h"
-/*
-int playout(Grille * g){
-  int t;
-  Coup liste[MaxCoup];
-  int nel;
-  Grille copie;
-
-  copie = * g;
-  while((t = cfini(&copie)) == PasFini){
-    nel = mkcoup(&copie, liste);
-    jouer(&copie, &liste[rand() % nel]);
-  }
-  return t;
-}
 
 
-
-
-Coup MC(Grille * g, int nplayout){
-  Coup liste[MaxCoup];
-  int nel;
-  int score[MaxCoup];
-  int i, imax;
-
-  nel = mkcoup(g, liste);
-
-  for(i = 0; i < nel; i++)
-    score[i] = 0;
-
-  // jouer des playouts et noter le résultat pour le premier coup
-  for(i = 0; i < nplayout; i++){
-    jouer(g, &liste[i % nel]);
-    score[i % nel] += playout(g);
-    dejouer(g, &liste[i % nel]);
-  }
-
-  // choisir celui avec le meilleur score
-  for(imax = 0, i = 1; i < nel; i++)
-    if ((g->kikijoue == R && score[i] > score[imax]) ||
-    (g->kikijoue == J && score[i] < score[imax]))
-      imax = i;
-
-  return liste[imax];
-}
-*/
-/*
- Test : combien de playouts par secondes ?
-*/
 int main(int ac, char * av[]){
   Grille mygrille = {Vide};
   char * line;
